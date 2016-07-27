48a2d4c0b6e4d740109f98dd3e175cb491edcbbc
HFCherish <hf_cherish@163.com>
2016-07-27 18:06:38 +0800

    [register user] try to save to db and can get one later

---
+++ src/main/java/com/thoughtworks/ketsu/domain/users/User.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.ketsu.domain.users;
+
+public class User {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
