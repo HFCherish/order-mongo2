71481ac04877ac32540b3e79b8f450fa69fb4ab8
HFCherish <hf_cherish@163.com>
2016-07-27 16:50:28 +0800

    [create product] return 201

--- src/test/java/com/thoughtworks/ketsu/support/TestHelper.java
+++ src/test/java/com/thoughtworks/ketsu/support/TestHelper.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.ketsu.support;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestHelper {
+    public static Map<String, Object> productJsonForTest() {
+        return new HashMap() {{
+            put("name", "Imran");
+            put("description", "teacher");
+            put("price", 678.1);
+        }};
+    }
 }
